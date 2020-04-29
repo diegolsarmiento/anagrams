16ff303c00bf38028710ce2ef6b54022cda652d6
Diego Sarmiento <44205748+diegolsarmiento@users.noreply.github.com>
2020-04-29 10:42:34 -0400

    Updated description

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+
+/*
+
+Anagrams: An anagram is a word or phrase 
+formed by rearranging the letters of a 
+different word or phrase, typically using all 
+the original letters exactly once.
+
+Examples
+
+Dormitory = Dirty room.
+School master = The classroom.
+Conversation = Voices rant on.
+Listen = Silent.
+Astronomer = Moon starer.
+The eyes = They see.
+A gentleman = Elegant man.
+Funeral = Real fun.
+
+*/
+
 function anagrams(str1, str2) {
 
     const array1 = helper(str1)
@@ -16,3 +37,8 @@ function helper(string) {
 }
 
 console.log(anagrams('this is a test', 'test this is a@@@@'));
+console.log(anagrams('Dormitory','Dirty room'));
+console.log(anagrams('School master','The classroom'));
+console.log(anagrams('Conversation','Voices rant on'));
+console.log(anagrams('Listen','Silent'));
+console.log(anagrams('Astronomer','Moon starer'));
